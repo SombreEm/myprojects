ed5a6dc5c6639b403b9ed5b70582ea71b674c818
SombreEm <133245293+SombreEm@users.noreply.github.com>
2023-11-30 20:49:37 +0200

    README

--- 30.11.2023/game/game/game.cpp
+++ 30.11.2023/game/game/game.cpp
@@ -1,68 +1,134 @@
 ﻿#include <iostream>
-#include <string>
-#include <Windows.h>
+
 using namespace std;
 
-struct Character {
-    string name;
-    int health;
-    int attack;
-    int level;
-    int experience;
+class Weapon {
+protected:
+    unsigned short damage;
+    unsigned short weight;
+    unsigned short numberOfCartridgesInTheClip;
+    unsigned short numberOfClips;
+    unsigned short accuracy; // 0.1 -> 0.9
+    double rateOfFire; // 0.1 -> 0.9
+public:
+    unsigned short getDamage() { return this->damage; }
+    unsigned short getWeight() { return this->weight; }
+    unsigned short getNumberOfCartridgesInTheClip() { return this->numberOfCartridgesInTheClip; }
+    unsigned short getNumberOfClips() { return this->numberOfClips; }
+    unsigned short getAccuracy() { return this->accuracy; }
+    double getRateOfFire() { return this->rateOfFire; }
 };
 
-void Attack(Character& attacker, Character& target) {
-    int damage = attacker.attack * attacker.level;
-    target.health -= damage;
-    cout << attacker.name << " наніс " << damage << " одиниць шкоди " << target.name << endl;
-}
-
-
-
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    Character player1 = { "Гравець 1", 100, 10, 1, 0 };
-    Character player2 = { "Гравець 2", 100, 10, 1, 0 };
-
-    while (player1.health > 0 && player2.health > 0) {
-        cout << "Гравець 1, виберіть дію:\n";
-        cout << "1. Атакувати\n";
-        cout << "2. Використати вміння\n";
-        int choice;
-        cin >> choice;
-
-        if (choice == 1) {
-            Attack(player1, player2);
-        }
-        else {
-            cout << "Невірний вибір дії. Спробуйте ще раз.\n";
-            continue;
-        }
-        cout << "Гравець 2, виберіть дію:\n";
-        cout << "1. Атакувати\n";
-        cout << "2. Використати вміння\n";
-        cin >> choice;
-
-        if (choice == 1) {
-            Attack(player2, player1);
-        }
-        else {
-            cout << "Невірний вибір дії. Спробуйте ще раз.\n";
-            continue;
-        }
-        cout << "Стан гравця 1: Здоров'я - " << player1.health << ", Рівень - " << player1.level << endl;
-        cout << "Стан гравця 2: Здоров'я - " << player2.health << ", Рівень - " << player2.level << endl;
+class Empty : public Weapon {
+public:
+    Empty() {
+        this->damage = 5;
+        this->weight = 0;
+        this->numberOfCartridgesInTheClip = 1000;
+        this->numberOfClips = 0;
+        this->accuracy = 0.6;
+        this->rateOfFire = 0.9;
     }
-    if (player1.health <= 0 && player2.health <= 0) {
-        cout << "Нічия!" << endl;
+};
+
+class Glock : public Weapon {
+public:
+    Glock() {
+        this->damage = 15;
+        this->weight = 25;
+        this->numberOfCartridgesInTheClip = 17;
+        this->numberOfClips = 5;
+        this->accuracy = 0.6;
+        this->rateOfFire = 0.7;
     }
-    else if (player1.health <= 0) {
-        cout << "Переміг Гравець 2!" << endl;
+};
+
+class AK47 : public Weapon {
+public:
+    AK47() {
+        this->damage = 55;
+        this->weight = 90;
+        this->numberOfCartridgesInTheClip = 30;
+        this->numberOfClips = 4;
+        this->accuracy = 0.7;
+        this->rateOfFire = 0.9;
     }
-    else {
-        cout << "Переміг Гравець 1!" << endl;
+};
+
+class M4A1S : public Weapon {
+public:
+    M4A1S() {
+        this->damage = 50;
+        this->weight = 80;
+        this->numberOfCartridgesInTheClip = 20;
+        this->numberOfClips = 4;
+        this->accuracy = 0.8;
+        this->rateOfFire = 0.9;
+    }
+};
+
+class ShotGun : public Weapon {
+public:
+    ShotGun() {
+        this->damage = 90;
+        this->weight = 80;
+        this->numberOfCartridgesInTheClip = 7;
+        this->numberOfClips = 4;
+        this->accuracy = 0.4;
+        this->rateOfFire = 0.6;
+    }
+};
+
+class Character {
+protected:
+    // unsigned short 0 -> 65535
+    unsigned short hp = 100;
+    unsigned short speed = 255;
+    unsigned short armor = 0;
+    unsigned short stamina = 0;
+    Weapon weapon;
+    void getW() {
+        Empty e;
+        Glock g;
+        if (rand()) {
+            this->weapon = e;
+        }
+        if (rand()) {
+            this->weapon = g;
+        }
+    }
+    void setRandomStaminaAndArmor() {
+        srand(time(0)); 
+        this->armor = rand() % 101;
+        this->stamina = rand() % 101;
     }
 
-    return 0;
+public:
+    Character() {
+        getW();
+        setRandomStaminaAndArmor();
+    }
+    void openFire(Character character) {
+        this->weapon.getDamage();
+    }
+};
+
+
+/*
+class Player : public Character {
+protected:
+    string name;
+public:
+    void setName(string name) { this->name = name; }
+    string getName() { return this->name; }
+
+};
+*/
+
+
+int main()
+{
+    Character first, second;
+    first.openFire(second);
+    second.openFire(first);
 }
