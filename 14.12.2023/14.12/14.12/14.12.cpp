4ad043f5fc664acccccd1e88b6773fe8072da66b
SombreEm <133245293+SombreEm@users.noreply.github.com>
2023-12-14 20:52:47 +0200

    README

---
+++ 14.12.2023/14.12/14.12/14.12.cpp
@@ -0,0 +1,42 @@
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    /*  vector<int> numbers = { 2, 5, 8, 12, 7, 3, 10, 6, 15, 9,
+                             20, 25, 18, 30, 35, 22, 28, 14, 16, 40,
+                             45, 33, 27, 21, 11, 36, 42, 24, 48, 50 };
+
+      auto normNum = [](int x) { return x % 2 == 0; };
+      vector<int> evenNumbers;
+      copy_if(numbers.begin(), numbers.end(), back_inserter(evenNumbers), normNum);
+
+      for (int num : evenNumbers) {
+          cout << num << " ";
+      }*/
+      //------------------------------------------------------------------------------------------------------
+   /* int line;
+    cin >> line;
+    cin.ignore();
+    vector<string> strings;
+    for (int i = 0; i < line; ++i) {
+        cout << i + 1 << ": ";
+        string inputString;
+        cin >> inputString;
+        strings.push_back(inputString);
+    }
+
+    sort(strings.begin(), strings.end(),
+        [](const string& a, const string& b) {
+            return a.length() < b.length();
+        });
+    for (const string& str : strings) {
+        cout << str << endl;
+    };*/
+    //-------------------------------------------------------------------------------------------------------------
+    
